863f38bd6fc4387d484a17aa1f2a7129ab951f82
aamir <wasi3aamir@gmail.com>
2019-09-22 22:41:17 +0600

    Course class added in Demo project with Author and List of Tag class

---
+++ Demo/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Author Author { get; set; }
+        public CourseLevel CourseLevel { get; set; }
+        public double Price { get; set; }
+        public IList<Tag> Tags { get; set; }
+    }
+}
