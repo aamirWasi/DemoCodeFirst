70e7b0a2b08fab976a9d40c5536cec98f1e18031
aamir <wasi3aamir@gmail.com>
2019-09-22 23:03:44 +0600

    filled Author class with a list of Courses

--- Demo/Author.cs
+++ Demo/Author.cs
@@ -1,6 +1,11 @@
-﻿namespace Demo
+﻿using System.Collections.Generic;
+
+namespace Demo
 {
     public class Author
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
     }
 }
