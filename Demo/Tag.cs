71ae0bddb751a80972142b70ee25b34ac1cb7321
aamir <wasi3aamir@gmail.com>
2019-09-22 23:06:47 +0600

    Tag added with a list of Courses

--- Demo/Tag.cs
+++ Demo/Tag.cs
@@ -1,6 +1,11 @@
-﻿namespace Demo
+﻿using System.Collections.Generic;
+
+namespace Demo
 {
     public class Tag
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
     }
 }
